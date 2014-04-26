f749bd0f8f26edcea63ad6ae4154c755b492b86f
jseg <seguela.julie@gmail.com>
2014-04-26 17:46:34 +0100

    do prog assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## Matrix inversion is usually a costly computation and their 
+## may be some benefit to caching the inverse of a matrix rather 
+## than compute it repeatedly. The two functions below are designed to
+## compute the inverse of a matrix by using a cache value system.
+
+
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL # initialize the inverse matrix with 'NULL'
+  set <- function(y) { 
+    x <<- y # set a new value to the 'x' matrix
+    s <<- NULL # if 'x' changes the value of 's' is removed
+  }
+  get <- function() x # return 'x' matrix value
+  setinv <- function(inv) s <<- inv # assign the value of inverse matrix to 's'
+  getinv <- function() s # return inverse matrix value
+  
+  # return the set, get, setinv and getinv functions
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix function above. If the inverse has already been calculated
+## then the cachesolve should retrieve the inverse from the cache.
+
+## Return a matrix that is the inverse of the special matrix 'x'.
+## We assume that the square matrix 'x' is always invertible.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  s <- x$getinv()
+  
+  # if the inverse has already been calculated, a cache value is found and returned
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  # else the value of x is get and the inverse is computed
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setinv(s) # assign inverse matrix value to 's' variable
+  s # return the value of inverse matrix
 }
